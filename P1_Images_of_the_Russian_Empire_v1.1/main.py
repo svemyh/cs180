1140b5fe8af45f4dcbdd435757195d3c80e1fbff
svemyh <s-myhre@outlook.com>
2024-09-09 16:21:16 -0700

    p1: rebuilding from scratch

---
+++ P1_Images_of_the_Russian_Empire_v1.1/main.py
@@ -0,0 +1,36 @@
+# p1 - Images of the Russian Empire
+# Sveinung Myhre <s.myhre@berkeley.edu>
+#
+# CS180/CS280 - starter code for project 1, available at: https://inst.eecs.berkeley.edu/~cs180/fa24/hw/proj1/
+
+import numpy as np
+import skimage as sk
+import skimage.io as skio
+from skimage.transform import pyramid_gaussian
+from utils import Aligner, crop_borders, remove_borders
+
+INPUT_IMAGE = "./data/cathedral.jpg"
+OUTPUT_IMAGE_PATH = "./output/out_colourized.jpg"
+
+if __name__ == "__main__":
+    ### Boilerplate / setup
+    im = skio.imread(INPUT_IMAGE)
+    height = int(np.floor(im.shape[0] / 3.0))
+    im = sk.img_as_float(im)
+    b = im[:height]
+    g = im[height: 2 * height]
+    r = im[2 * height: 3 * height]
+
+    ### 
+    aligner = Aligner
+    ar = aligner.simple_align(r, g)
+    #ag = aligner.simple_align(g, g)
+    ag = g
+    ab = aligner.simple_align(b, g)
+
+    ar, ag, ab = remove_borders(ar, ag, ab)
+
+    im_out = np.dstack([ar, ag, ab])
+
+    skio.imshow(im_out)
+    skio.show()
---
+++ P1_Images_of_the_Russian_Empire_v1.1/utils.py
@@ -0,0 +1,74 @@
+import numpy as np
+
+class Aligner:
+    def dummy_align(self, base_img, target_img):
+        """Handy for testing purposes. Returns the base image."""
+        return base_img
+    
+    def simple_align(_img_1, _img_2, N=15):
+        """Searches over a window of [-N,N] possible translational pixel displacements."""
+        best_value = float("inf")
+        best_dx, best_dy = 0, 0
+
+        img_1 = crop_borders(_img_1, 0.1)
+        img_2 = crop_borders(_img_2, 0.1)
+
+        for dx in range(-N, N):
+            for dy in range(-N, N):
+                ssd = custom_metric(img_translate(img_1, dx, dy), img_2)
+                if ssd < best_value:
+                    best_value = ssd
+                    best_dx, best_dy = dx, dy
+
+        print("Alignment:")
+        print(f"dx = {best_dx}")
+        print(f"dy = {best_dy}")
+
+        return img_translate(_img_1, best_dx, best_dy)
+    
+
+def crop_borders(img, percentage):
+    x_crop = int(percentage * img.shape[0] / 2)
+    y_crop = int(percentage * img.shape[1] / 2)
+    return img[x_crop:img.shape[0]-x_crop, y_crop:img.shape[1]-y_crop]
+
+
+def img_translate(img, dx, dy):
+    result = np.roll(img, dx, axis=1)
+    result = np.roll(result, dy, axis=0)
+    return result
+
+
+def custom_metric(img_1, img_2):
+    score = 0
+    score += sum_of_squared_diff(img_1, img_2)
+    return score
+
+def sum_of_squared_diff(img_1, img_2):
+    return np.sum((img_1 - img_2) ** 2)
+
+
+def remove_borders(r, g, b, percentage=0.9):
+    """Remove borders such that only the overlapping parts remain. Solution could be more or less intelligent"""
+    assert r.shape == g.shape == b.shape
+    max_height, max_width = r.shape
+    r = crop_image_from_center(
+        r, (max_height // 2, max_width // 2), int(percentage * min(max_height, max_width) / 2)
+    )
+    g = crop_image_from_center(
+        g, (max_height // 2, max_width // 2), int(percentage * min(max_height, max_width) / 2)
+    )
+    b = crop_image_from_center(
+        b, (max_height // 2, max_width // 2), int(percentage * min(max_height, max_width) / 2)
+    )
+    return r, g, b
+
+def crop_image_from_center(img, center, circumradius: int):
+    """Crop image by declaring a centr pixel coordinate then grabbing the 'circumradius' amount of pixels above it, below, to its right and elft."""
+    center_row, center_col = center
+
+    cropped_img = img[
+        center_row - circumradius : center_row + circumradius,
+        center_col - circumradius : center_col + circumradius,
+    ]
+    return cropped_img
